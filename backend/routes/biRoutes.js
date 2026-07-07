2559a5f612d52bb9e32d2b9d5f23fc69dc3c7c19
SURYA-200346 <surya.jsdev@gmail.com>
2026-07-08 00:24:52 +0530

    error fixed

--- backend/routes/biRoutes.js
+++ backend/routes/biRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getBIData, createBIReport } from '../controllers/biController.js';
-import { checkManagerAccess } from '../middleware/checkManagerAccess.js';
+
 
 const router = express.Router();
 
